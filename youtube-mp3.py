dd1773a57d9e88657f3cbb0a72abd23c8b7cf728
dizcza <dizcza@gmail.com>
2022-08-20 21:12:25 +0300

    skip downloaded videos on retry

--- youtube-mp3.py
+++ youtube-mp3.py
@@ -1,14 +1,18 @@
 from __future__ import unicode_literals
 import youtube_dl
 import argparse
+from pathlib import Path
+import tempfile
 
 parser = argparse.ArgumentParser()
 parser.add_argument("url", help="youtube video url")
 args = parser.parse_args()
+archive_txt = str(Path(tempfile.gettempdir()) / "youtube_archive.txt")
 
 ydl_opts = {
     'outtmpl': "~/Downloads/%(title)s.%(ext)s",
     'format': 'bestaudio/best',
+    'download_archive': archive_txt,
     'postprocessors': [{
         'key': 'FFmpegExtractAudio',
         'preferredcodec': 'mp3',
